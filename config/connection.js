9f4b14306a0a71ad4601ca16439a15faa5f32015
Arielle <ariellcollinsb@gmail.com>
2019-10-16 23:10:25 -0600

    adding code fixes relative to routing

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,7 @@ var connection = mysql.createConnection({
     password: "root",
     database: "burgers_db"
   });
-  
-  // Make connection.
+
   connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
@@ -16,6 +15,5 @@ var connection = mysql.createConnection({
     }
     console.log("connected as id " + connection.threadId);
   });
-  
-  // Export connection for our ORM to use.
+
   module.exports = connection;
